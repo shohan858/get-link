8dd59f49ba4535f0984e96c6209d52d6c94aafd8
Akikaze235 <AkikazeNoQwaserQwaser@gmail.com>
2023-03-30 09:00:54 +0700

    hapus bos

--- getlink-dinamis-main/resources/views/budget.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('admin.base')
-@section('konten')
-@foreach ($data as $item)
-    <h1>saldo anda adalah: </h1>
-    {{ $item->value }}
-@endforeach
-@endsection
--- getlink-dinamis-main/resources/views/tampil_template.blade.php
+++
@@ -1,6 +0,0 @@
-<style>
-    body{
-        margin: 0; padding: 0;
-    }
-</style>
-<iframe src="{{ asset('microsite_template/template1.blade.php') }}" style="width: 100vw; height: 100vh;" type="">
