8c644e489ce7eeeb00fd19b9247309115b3aaabc
SandyPrida <sandyprida20@gmail.com>
2023-04-04 11:13:02 +0700

    halaman_adminReal

---
+++ frontend(sandy)/halaman_admin2/storage/framework/views/d6d05c6864e8512ccaaf7380486690aa.php
@@ -0,0 +1,9 @@
+<?php $__env->startSection("admin_konten"); ?>
+
+<div class="adDash">
+
+</div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make("admin.layout.base", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\FrondEnd\quest\GetLink\halaman_admin\resources\views/admin/admin_pages/microsite.blade.php ENDPATH**/ ?>
