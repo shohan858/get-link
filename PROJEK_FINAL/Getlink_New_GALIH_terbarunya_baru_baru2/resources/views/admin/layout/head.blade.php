74a1c8fc145dbc30e2ae49e86bb73d7d56af0e56
Akikaze235 <AkikazeNoQwaserQwaser@gmail.com>
2023-04-12 09:26:36 +0700

    update shortlink

---
+++ PROJEK_FINAL/Getlink_New_GALIH_terbarunya_baru_baru2/resources/views/admin/layout/head.blade.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="{{ asset('css/style_admin.css') }}">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="{{asset('DataTables/datatables.css')}}">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css"> --}}
+    {{-- <link rel="stylesheet" href="{{asset('DataTables/datatables.min.css')}}"> --}}
+    {{-- <link href="https://cdn.datatables.net/v/dt/dt-1.13.4/datatables.min.css" rel="stylesheet"/> --}}
+    {{-- <link href="{{asset("DataTables/datatables.min.css")}}" rel="stylesheet"/> --}}
+    {{-- <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css"> --}}
+</head>
---
+++ PROJEK_FINAL/Getlink_New_GALIH_terbarunya_baru_baru2/resources/views/layout/Dashboard_User/base.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  @include('layout.Dashboard_user._nav')
+  <body>
+   @include('layout.Dashboard_User._head')
+
+    @yield('konten')
+
+    @include('layout.Dashboard_User._aside')
+    <!-- @include('layout.Dashboard_user.scripts') -->
+  </body>
+</html>
